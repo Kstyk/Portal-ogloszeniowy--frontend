d2c1eb9dc6fc66f299653d22ae1c851485949325
Seweryn Drag <dragseweryn@zohomail.eu>
2023-06-28 16:31:30 +0200

    Dodanie react router, objęcie całej aplikacji BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import NavbarPrincipal from "./components/NavbarPrincipal";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div id="container" className="container w-7/12 m-auto">
+      <NavbarPrincipal />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
