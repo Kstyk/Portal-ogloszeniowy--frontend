abdd20c55c35b484982b8dbfbf8485f282cdcf6a
Seweryn Drag <dragseweryn@zohomail.eu>
2023-06-28 11:37:08 +0200

    Stworzenie szkieletu projektu

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+export default App;
