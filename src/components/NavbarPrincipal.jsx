d394f990837be41ae86d14a50fff46ffd8fa8f8c
Seweryn Drag <dragseweryn@zohomail.eu>
2023-06-28 16:30:21 +0200

    Stowrzenie wstępnego navbara dla zleceniodawcy

---
+++ src/components/NavbarPrincipal.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const NavbarPrincipal = () => {
+  return (
+    <div className="navbar mb-5" data-theme="light">
+      <div className="flex-1">
+        <a
+          className="btn border-none bg-white hover:bg-white normal-case text-xl"
+          href="/"
+        >
+          <img src="src/assets/images/logo.png" alt="" className="h-full" />
+        </a>
+      </div>
+      <div className="flex-none gap-2">
+        <div className="form-control pr-5 mr-5 border-r-2">Cześć, username</div>
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src="src/assets/images/avatar.jpg" alt="profile-image" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">Twój profil</a>
+            </li>
+            <li>
+              <a>Twoje zlecenia</a>
+            </li>
+            <li>
+              <a>Edytuj profil</a>
+            </li>
+            <hr />
+            <li>
+              <a>Wyloguj</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarPrincipal;
