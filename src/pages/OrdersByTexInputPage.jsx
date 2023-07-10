47c4bb41e060d646e50fdc926dcbc68d49fce136
Seweryn Drag <dragseweryn@zohomail.eu>
2023-07-10 14:11:57 +0200

    Wydzielenie listy województw do osobnego pliku

--- src/pages/OrdersByTexInputPage.jsx
+++ src/pages/OrdersByTexInputPage.jsx
@@ -4,10 +4,10 @@ import Select from "react-select";
 import useAxios from "../hooks/useAxios";
 import { useState } from "react";
 import OrderCard from "../components/OrdersListPageComponents/OrderCard";
-import FilterMobile from "../components/OrdersListPageComponents/FilterMobile";
 import { Link } from "react-router-dom";
 import FilterMobileByTextInput from "../components/OrdersListPageComponents/FilterMobileByTextInput";
 import LoadingComponent from "../components/LoadingComponent";
+import voivodeships from "../components/content/Voivodeships";
 
 const OrdersByTexInputPage = () => {
   const { searchByQuery } = useParams();
@@ -48,25 +48,6 @@ const OrdersByTexInputPage = () => {
   const options = [
     { value: "zlecenia", label: "zleceń" },
     { value: "wykonawcy", label: "wykonawców" },
-  ];
-
-  const voivodeships = [
-    { value: "dolnośląskie", label: "dolnośląskie" },
-    { value: "kujawskoPomorskie", label: "kujawsko-pomorskie" },
-    { value: "lubelskie", label: "lubelskie" },
-    { value: "lubuskie", label: "lubuskie" },
-    { value: "łódzkie", label: "łódzkie" },
-    { value: "małopolskie", label: "małopolskie" },
-    { value: "mazowieckie", label: "mazowieckie" },
-    { value: "opolskie", label: "opolskie" },
-    { value: "podkarpackie", label: "podkarpackie" },
-    { value: "podlaskie", label: "podlaskie" },
-    { value: "pomorskie", label: "pomorskie" },
-    { value: "śląskie", label: "śląskie" },
-    { value: "świętokrzyskie", label: "świętokrzyskie" },
-    { value: "warmińskoMazurskie", label: "warmińsko-mazurskie" },
-    { value: "wielkopolskie", label: "wielkopolskie" },
-    { value: "zachodnioPomorskie", label: "zachodnio-pomorskie" },
   ];
 
   const fetchResultCategories = async () => {
